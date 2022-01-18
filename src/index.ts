bd6639f8be50a18af354a2badfa57b75cb8cc92c
dedi-dev <id.dedi81@gmail.com>
2022-01-18 22:53:37 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import { routes } from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({origin: ["http://localhost:3000"]}));
+
+routes(app);
+
+app.listen(8000, () => {
+    console.log("Server running on port 8000");
+});
